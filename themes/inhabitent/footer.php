5662c6371392d58eb97dcd7a2be2410055bfab4c
qingxin-yuan <qingxin.yn@gmail.com>
2017-11-13 14:20:37 -0800

    footer layout updated

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -9,9 +9,25 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="footer-background-image" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/images/dark-wood.png');">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+					<div class="contact-info">
+						<h3>contact info</h3>
+						<p><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i>778-456-7891</p>
+						<p><i class="fa fa-facebook-square" aria-hidden="true"></i><i class="fa fa-twitter-square" aria-hidden="true"></i><i class="fa fa-google-plus-square" aria-hidden="true"></i></p>
+					</div>
+					<div class="business-hours">
+						<h3>business hours</h3>
+						<p><span class="text-bold">Monday - Friday:</span> 9am to 5pm</p>
+						<p><span class="text-bold">Saturday:</span> 10am to 2pm</p>
+						<p><span class="text-bold">Sunday:</span> Closed</p>
+					</div>
+					<div class="footer-image">
+						<a href=""><img src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-text.svg"></a>
+					</div>
+				</div>
+			</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
