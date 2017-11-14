6bd5fd6540e9375837c285d754edf501497ba131
qingxin-yuan <qingxin.yn@gmail.com>
2017-11-14 15:20:29 -0800

    custom fileds added for about page, about hero image uploaded

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -11,6 +11,34 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
+function inhabitent_dynamic_css(){
+	if(!is_page_template('page-templates/about.php')){
+		return;
+	}
+	$image = CFS()->get('about_header_image');
+
+	if (!$image){
+		return;
+	}
+
+	$hero_css=".page-template-about .entry-header{
+		height: 100vh;
+		background:
+			linear-gradient(to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100%), url({$image}) no-repeat center bottom;
+		background-size: cover, cover;
+		
+
+	}";
+
+	wp_add_inline_style('inhabitent-style',$hero_css);
+}
+add_action('wp_enqueue_scripts','inhabitent_dynamic_css');
+
+
+
+
+
+
 function inhabitent_body_classes( $classes ) {
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
